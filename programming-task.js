0c3e79a66bcbfc06fe51f686b31c05d0f9b78965
MahmudExtra <picowf@gmail.com>
2023-03-23 03:37:58 +0600

    code optimised

--- programming-task.js
+++ programming-task.js
@@ -1,48 +1,47 @@
-// I solved this problem using 3 different ways.Two is commented out. You can uncomment them and Test them.
+// I solved this problem using 2 different ways.Two is commented out. You can uncomment them and Test them.
 
 // solution 1
 
-const vaxTrail = (arr) => {
+const vaxTrail = (inputData) => {
   const instance = {};
 
-  arr.forEach((item) => {
+  inputData.forEach((item) => {
     if (item.age >= 20 && item.age <= 30 && item.temperature < 100) {
       if (instance.A) {
         instance.A.push(item);
-        instance.A.sort((a, b) => a.age - b.age);
       } else {
         instance.A = [item];
       }
     } else if (item.age >= 31 && item.age <= 40 && item.temperature < 100) {
       if (instance.B) {
         instance.B.push(item);
-        instance.B.sort((a, b) => a.age - b.age);
       } else {
         instance.B = [item];
       }
     } else if (item.age >= 41 && item.age <= 50 && item.temperature < 100) {
       if (instance.C) {
         instance.C.push(item);
-        instance.C.sort((a, b) => a.age - b.age);
       } else {
         instance.C = [item];
       }
     } else if (item.temperature >= 100 && item.age > 0) {
       if (instance.D) {
         instance.D.push(item);
-        instance.D.sort((a, b) => a.age - b.age);
       } else {
         instance.D = [item];
       }
     }
   });
 
+  for (let key in instance) {
+    instance[key].sort((a, b) => a.age - b.age);
+  }
   return instance;
 };
 
 // solution 2
 
-// const vaxTrail = (arr) => {
+// const vaxTrail = (inputData) => {
 //   const instance = {
 //     A: [],
 //     B: [],
@@ -50,7 +49,7 @@ const vaxTrail = (arr) => {
 //     D: [],
 //   };
 
-//   arr.forEach((item) => {
+//   inputData.forEach((item) => {
 //     if (item.age >= 20 && item.age <= 30 && item.temperature < 100) {
 //       instance.A.push(item);
 //     } else if (item.age >= 31 && item.age <= 40 && item.temperature < 100) {
@@ -62,54 +61,22 @@ const vaxTrail = (arr) => {
 //     }
 //   });
 
-//   const ans = {
-//     A: instance.A.sort((a, b) => a.age - b.age),
-//     B: instance.B.sort((a, b) => a.age - b.age),
-//     C: instance.C.sort((a, b) => a.age - b.age),
-//     D: instance.D.sort((a, b) => a.age - b.age),
-//   };
-
-//   return ans;
-// };
-
-// solution 3
-
-// const vaxTrail = (arr) => {
-//   const instance = {
-//     A: [],
-//     B: [],
-//     C: [],
-//     D: [],
-//   };
-
-//   arr.forEach((item) => {
-//     if (item.age >= 20 && item.age <= 30 && item.temperature < 100) {
-//       instance.A.push(item);
-//       instance.A.sort((a, b) => a.age - b.age);
-//     } else if (item.age >= 31 && item.age <= 40 && item.temperature < 100) {
-//       instance.B.push(item);
-//       instance.B.sort((a, b) => a.age - b.age);
-//     } else if (item.age >= 41 && item.age <= 50 && item.temperature < 100) {
-//       instance.C.push(item);
-//       instance.C.sort((a, b) => a.age - b.age);
-//     } else if (item.temperature >= 100 && item.age > 0) {
-//       instance.D.push(item);
-//       instance.D.sort((a, b) => a.age - b.age);
-//     }
-//   });
+//   for (let key in instance) {
+//     instance[key].sort((a, b) => a.age - b.age);
+//   }
 
 //   return instance;
 // };
 
-// console.log(
-//   vaxTrail([
-//     { name: "Biplap", age: 22, temperature: 98 },
-//     { name: "sunil", age: 21, temperature: 98 },
-//     { name: "Rahul", age: 37, temperature: 99 },
-//     { name: "Kabir", age: 36, temperature: 99 },
-//     { name: "Paul", age: 42, temperature: 98 },
-//     { name: "Kat", age: 41, temperature: 98 },
-//     { name: "Nayem", age: 51, temperature: 100 },
-//     { name: "Sabnaj", age: 50, temperature: 101 },
-//   ])
-// );
+console.log(
+  vaxTrail([
+    { name: "Biplap", age: 22, temperature: 98 },
+    { name: "sunil", age: 21, temperature: 98 },
+    { name: "Rahul", age: 37, temperature: 99 },
+    { name: "Kabir", age: 36, temperature: 99 },
+    { name: "Paul", age: 42, temperature: 98 },
+    { name: "Kat", age: 41, temperature: 98 },
+    { name: "Nayem", age: 51, temperature: 100 },
+    { name: "Sabnaj", age: 50, temperature: 101 },
+  ])
+);
